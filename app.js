7f510642b2ed2b10034a68f8ea08d68ffcc129b5
Afshan kanwal <kanwalafshan2244@gmail.com>
2025-05-16 10:01:49 +0500

    rainbow-color

---
+++ app.js
@@ -0,0 +1,28 @@
+const colorBox = document.getElementById('colorBox');
+ const hoverSound = document.getElementById('hoverSound');
+ let audioUnlocked = false;
+document.body.addEventListener('click', () => {
+      if (!audioUnlocked) {
+        hoverSound.play().then(() => {
+          hoverSound.pause();
+          hoverSound.currentTime = 0;
+          audioUnlocked = true;
+          console.log(" Audio unlocked by body click");
+        }).catch(e => console.log("Unlock error:", e));
+      }
+    });
+  
+  colorBox.addEventListener('mousemove', function() {
+    colorBox.style.background = `linear-gradient(${Math.random() * 360}deg, red, orange, yellow, green, blue, indigo, violet)`;
+      
+  });
+   colorBox.addEventListener('mouseenter', function(){
+     hoverSound.currentTime = 0;
+      hoverSound.play().catch(e => console.log("Play error:", e));
+    });
+
+  colorBox.addEventListener('mouseleave', function() {
+    colorBox.style.background = '#333'; 
+     hoverSound.pause();
+      hoverSound.currentTime = 0;
+  });
